b414002b649e2154d06785bbded3d435096341de
mawonde <mawondep@gmail.com>
2023-12-08 11:33:34 +0000

    fixed pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 """Type annotations on an add function """
+
+
 def add(a: float, b: float) -> float:
-  """
-  Adds two float numbers and returns the result.
-  """
-  return a + b
+    """
+    Adds two float numbers and returns the result.
+    """
+    return a + b
